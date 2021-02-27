bb8aedcda1a91491f5235c71cfc7102a8a98f92a
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-26 16:58:46 -0800

    Remove notes and finalize project

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,9 +1,3 @@
-# check two words against eachother
-# account for diff cases
-# check words to ensure real, return statement if no vowels
-# check if no letters match for antigram and return statement
-# account for sentences and remove spaces/punctuation, only look at the words.
-
 class Words
   def initialize(string_1, string_2)
     @string_1 = string_1
